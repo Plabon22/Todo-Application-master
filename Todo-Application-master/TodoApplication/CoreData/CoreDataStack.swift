f9bd2784dad35bf2e98a1d7cc57b17c9f3f43542
Plabon Shekh <Plabon.csedu22@gmail.com>
2019-01-30 22:25:59 +0600

    final project

---
+++ Todo-Application-master/TodoApplication/CoreData/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  TodoApplication
+//
+//  Created by Plabon Shekh on 12/11/2018.
+//  Copyright © 2018 Plabon Shekh. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    var container: NSPersistentContainer {
+        let container = NSPersistentContainer(name: "Todos")
+        container.loadPersistentStores { (description, error) in
+            guard error == nil else {
+                print("Error: \(error!)")
+                return
+            }
+        }
+        
+        return container
+    }
+
+    var managedContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
